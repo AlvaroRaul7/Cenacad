cd41f33b1e3eee1d62e71471efa55b1775830784
LuisOToro <lotoro@espol.edu.ec>
2020-01-23 21:12:52 -0500

    correlacion y extraccion de factores

--- procesamiento.R
+++ procesamiento.R
@@ -2,20 +2,19 @@ library(float)
 completo <- read.csv2("C:/Users/Luis Toro/Documents/GitHub/Cenacad/completo.csv",header = F)
 names(completo) = c("anio","semestre","codigo_materia","materia","paralelo","profesor","pregunta","media","desviacion")
 general <- cbind.data.frame(completo$pregunta,as.numeric(levels(completo$media))[completo$media],as.numeric(levels(completo$desviacion))[completo$desviacion],stringsAsFactors = FALSE)
-names(general)=c("Pregunta","Media","Desviacion")
+nombres <- c("Pregunta","Media","Desviacion")
+names(general)= nombres
 summary(general)
 str(general)
-#is.atomic(general$Media)
-#cor(general$Media,general$Desviacion)
-plot(general$Pregunta,general$Media)
-plot(general$Pregunta,general$Desviacion)
-#cor.test(general)
+cor(general$Media,general$Desviacion)
+plot(general$Pregunta,general$Media,main = "grafico de preguntas vs calificacion",ylab = "calificacion",xlab="preguntas")
+plot(general$Pregunta,general$Desviacion,main = "grafico de preguntas vs diferencia de calificacion",ylab = "diferencia de calificacion",xlab="preguntas")
 hist(general$Media)
 hist(general$Desviacion)
 #hist(general$Desviacion)
 preguntas <- as.factor(general$Pregunta)
 summary(preguntas)
-pregunta1<-cbind.data.frame(general$Pregunta[general$Pregunta==preguntas[1]],as.numeric(levels(general$Media[general$Pregunta==preguntas[1]]))[general$Media[general$Pregunta==preguntas[1]]],as.numeric(levels(general$Desviacion[general$Pregunta==preguntas[1]]))[general$Desviacion[general$Pregunta==preguntas[1]]])
+pregunta1<-cbind.data.frame(general$Pregunta[general$Pregunta==preguntas[1]],general$Media[general$Pregunta==preguntas[1]],general$Desviacion[general$Pregunta==preguntas[1]])
 pregunta2<-cbind.data.frame(general$Pregunta[general$Pregunta==preguntas[2]],general$Media[general$Pregunta==preguntas[2]],general$Desviacion[general$Pregunta==preguntas[2]])
 pregunta3<-cbind.data.frame(general$Pregunta[general$Pregunta==preguntas[3]],general$Media[general$Pregunta==preguntas[3]],general$Desviacion[general$Pregunta==preguntas[3]])
 pregunta4<-cbind.data.frame(general$Pregunta[general$Pregunta==preguntas[4]],general$Media[general$Pregunta==preguntas[4]],general$Desviacion[general$Pregunta==preguntas[4]])
@@ -27,6 +26,38 @@ pregunta9<-cbind.data.frame(general$Pregunta[general$Pregunta==preguntas[9]],gen
 pregunta10<-cbind.data.frame(general$Pregunta[general$Pregunta==preguntas[10]],general$Media[general$Pregunta==preguntas[10]],general$Desviacion[general$Pregunta==preguntas[10]])
 pregunta11<-cbind.data.frame(general$Pregunta[general$Pregunta==preguntas[11]],general$Media[general$Pregunta==preguntas[11]],general$Desviacion[general$Pregunta==preguntas[11]])
 pregunta12<-cbind.data.frame(general$Pregunta[general$Pregunta==preguntas[12]],general$Media[general$Pregunta==preguntas[12]],general$Desviacion[general$Pregunta==preguntas[12]])
-(pregunta1$`general$Media[general$Pregunta == preguntas[1]]`)
-as.numeric(levels(pregunta1$`general$Media[general$Pregunta == preguntas[1]]`))
+names(pregunta1)=nombres
+names(pregunta2)=nombres
+names(pregunta3)=nombres
+names(pregunta4)=nombres
+names(pregunta5)=nombres
+names(pregunta6)=nombres
+names(pregunta7)=nombres
+names(pregunta8)=nombres
+names(pregunta9)=nombres
+names(pregunta10)=nombres
+names(pregunta11)=nombres
+names(pregunta12)=nombres
 summary(pregunta1)
+media1 = mean(pregunta1$Media)
+media2 = mean(pregunta2$Media)
+media3 = mean(pregunta3$Media)
+media4 = mean(pregunta4$Media)
+media5 = mean(pregunta5$Media)
+media6 = mean(pregunta6$Media)
+media7 = mean(pregunta7$Media)
+media8 = mean(pregunta8$Media)
+media9 = mean(pregunta9$Media)
+media10 = mean(pregunta10$Media)
+media11 = mean(pregunta11$Media)
+media12 = mean(pregunta12$Media)
+#Agrupacion de las preguntas que consideremos para cada factor que elegimos
+#Calidad de la enseñanza del profesor y como lo percibe el estudiante
+factor1 <- cbind(pregunta1$Media,pregunta2$Media,pregunta4$Media,pregunta5$Media)
+#destreza pedagógica del profesor
+factor2 <- cbind(pregunta2$Media,pregunta4$Media)
+#entorno
+factor3 <- cbind(pregunta3$Media,pregunta6$Media,pregunta7$Media,pregunta8$Media)
+cor(factor1)
+cor(factor2)
+cor(factor3)
